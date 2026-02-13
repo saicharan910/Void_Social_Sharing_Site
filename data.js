f95b49101838ad7d719412220c6c280bd7a6eb44
saicharan910 <thecharan122004@gmail.com>
2026-02-13 11:25:00 +0530

    Add USERS and posts data structure

---
+++ data.js
@@ -0,0 +1,9 @@
+const USERS = {
+  elena: { id: 'elena', name: 'elena morse', handle: '@elena', av: 'em' },
+  kai: { id: 'kai', name: 'kai nakamura', handle: '@kainak', av: 'kn' }
+};
+
+let posts = [
+  { id: 1, uid: 'elena', text: "minimalism is the presence of the right things.", time: '2m', likes: 241, liked: false, replies: 12 },
+  { id: 2, uid: 'kai', text: "the machine always wins. #code", time: '14m', likes: 87, liked: false, replies: 5 }
+];
