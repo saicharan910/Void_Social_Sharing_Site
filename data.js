a0cd1483109338076b6b729af420e0d266a748e2
saicharan910 <thecharan122004@gmail.com>
2026-02-13 11:39:40 +0530

    Add new user Sai and post to data.js

--- data.js
+++ data.js
@@ -1,9 +1,11 @@
 const USERS = {
   elena: { id: 'elena', name: 'elena morse', handle: '@elena', av: 'em' },
-  kai: { id: 'kai', name: 'kai nakamura', handle: '@kainak', av: 'kn' }
+  kai: { id: 'kai', name: 'kai nakamura', handle: '@kainak', av: 'kn' },
+  Sai: {id: 'sai', name: 'sai charan' , handle: '@saicharan', av: 'sc'}
 };
 
 let posts = [
   { id: 1, uid: 'elena', text: "minimalism is the presence of the right things.", time: '2m', likes: 241, liked: false, replies: 12 },
   { id: 2, uid: 'kai', text: "the machine always wins. #code", time: '14m', likes: 87, liked: false, replies: 5 }
+  { id: 3, uid: 'sai', text: "Life's worth with peace.", time: '14m', likes: 69, liked: false, replies: 15 }
 ];
